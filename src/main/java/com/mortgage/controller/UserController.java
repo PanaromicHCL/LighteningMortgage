40601b91982c5e0c213f4e60e9f280e363c655d7
techfreks <48522802+techfreks@users.noreply.github.com>
2019-03-18 16:39:37 -0700

    add controller

--- src/main/java/com/mortgage/controller/UserController.java
+++ src/main/java/com/mortgage/controller/UserController.java
@@ -1,5 +1,19 @@
 package com.mortgage.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api")
+@CrossOrigin
 public class UserController {
 
+	Logger logger = LoggerFactory.getLogger(UserController.class);
+
+	
+	
 }
